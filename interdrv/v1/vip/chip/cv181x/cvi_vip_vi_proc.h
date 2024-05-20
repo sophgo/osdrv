82894de41fa757450505960e4ba1c9aeba847a3d
yike2024 <ke.yi@sophgo.com>
2024-05-20 12:11:12 +0800

    update 1.6

--- interdrv/v1/vip/chip/cv181x/cvi_vip_vi_proc.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CVI_VIP_VI_PROC_H_
-#define _CVI_VIP_VI_PROC_H_
-
-#include "cvi_vip_isp.h"
-
-int vi_proc_init(struct cvi_isp_vdev *_vdev, void *shm);
-int vi_proc_remove(void);
-
-#endif // _CVI_VIP_VI_PROC_H_
