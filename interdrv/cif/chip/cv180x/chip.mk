27418fa9d0bcf499fc5517497eb5b9c4d5e186c4
sophgo-forum-service <forum_service@sophgo.com>
2025-12-17 14:48:19 +0800

    osdrv: weekly rls 2025.12.17
    669486, [fix](vi): fix ai bnr dumpraw fail
    bfd109, [fix](VI): remove ai isp out ion for Mars
    96f99a, [feat](vi): fix the problem of ldci/drc module flashing when rgb(wdr)+yuv

--- interdrv/cif/chip/cv180x/chip.mk
+++
@@ -1,2 +0,0 @@
-cvi_mipi_rx-objs += chip/$(CVIARCH_L)/cif.o
-cvi_mipi_rx-objs += chip/$(CVIARCH_L)/drv/cif_drv.o
--- interdrv/rgn/chip/cv180x/chip.mk
+++
@@ -1,4 +0,0 @@
-$(CVIARCH_L)_rgn-objs += chip/$(CVIARCH_L)/rgn.o
-$(CVIARCH_L)_rgn-objs += chip/$(CVIARCH_L)/vip/rgn_drv.o
-$(CVIARCH_L)_rgn-objs += chip/$(CVIARCH_L)/proc/rgn_proc.o
-
