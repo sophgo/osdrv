3238e5f325c09b2ea226a36fa60f30b5ec700c67
琳 何 <lin.he@sophgo.com>
2026-06-01 14:17:07 +0800

    Revert "add hw_yuv_auto_csc function to set reg_auto_csc_en for HK demand"
    This reverts commit 6426f7a94446493b45108739cdface6ad9bf8934.
    
    Change-Id: I996bfab0b56689430f8f5fb2b0cd6fcf8a19e839

--- interdrv/v2/vpss/chip/soph/vpss_hal.h
+++ interdrv/v2/vpss/chip/soph/vpss_hal.h
@@ -124,7 +124,6 @@ struct vpss_hal_chn_cfg {
 	struct sc_border_vpp_param border_vpp_cfg[VPSS_RECT_NUM];
 	struct sc_mute mute_cfg;
 	struct csc_cfg csc_cfg;
-	bool hw_yuv_auto_csc;
 	enum sc_flip_mode flip;
 	enum sc_scaling_coef sc_coef;
 };
@@ -134,7 +133,6 @@ struct vpss_hal_grp_cfg {
 	bool online_from_isp;
 	bool upsample;
 	bool bm_scene;
-	bool hw_yuv_auto_csc;
 	__u32 pixelformat;
 	__u32 bytesperline[2];
 	__u64 addr[4];
